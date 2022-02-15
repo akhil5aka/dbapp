761ebe30ba5a8bbf1421978e6a035e257d6f32e9
sadasivanks <sadasadasivan2000@gmail.com>
2022-02-15 15:58:52 +0530

    db app

---
+++ app/src/main/java/com/example/studentdbapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.studentdbapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+AppCompatButton b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(AppCompatButton) findViewById(R.id.adas);
+        b2=(AppCompatButton) findViewById(R.id.ss);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),SearchStudent.class);
+            startActivity(i);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),AddStudent.class);
+                startActivity(i);
+            }
+        });
+    }
+}
